498a4bb1213915c29acbf23475dbbeb3c48cc951
owolana ayomide <ayomideowolana1@gmail.com>
2022-03-07 05:38:33 +0100

    addes index for javascript

---
+++ pages/Courses/javascript/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../../components/header"
+
+
+const Introduction = () => {
+    return ( 
+        <div>
+            <Header text="Introduction to Javascript" />
+        </div>
+     );
+}
+ 
+export default Introduction;
